fd023d42f3e60c7b5f27495d5a31d2088d58049b
Benny Li <benny@benny-li.de>
2015-11-27 22:18:33 +0100

    Introducing Ansible for nginx + fix

--- RaspberryPi/nginx/entrypoint.sh
+++ RaspberryPi/nginx/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-sed -ni "s|DOCKER_PHP_PORT|${PHP_PORT}|g" /etc/nginx/global/php-upstream.conf
-sed -ni "s|DOCKER_REDMINE_PORT|${REDMINE_PORT}|g" /etc/nginx/global/redmine-upstream.conf
-sed -ni "s|DOCKER_GHOST_PORT|${GHOST_PORT}|g" /etc/nginx/global/ghost-blog-upstream.conf
-$@
+PHP_UPSTREAM_CONF_FILE="/etc/nginx/global/php-upstream.conf"
+if [ -f "$PHP_UPSTREAM_CONF_FILE" ]; then
+  sed -ni "s|DOCKER_PHP_PORT|${PHP_PORT}|g" "$PHP_UPSTREAM_CONF_FILE"
+fi
+
+REDMINE_UPSTREAM_CONF_FILE="/etc/nginx/global/redmine-upstream.conf"
+if [ -f "REDMINE_UPSTREAM_CONF_FILE" ]; then
+  sed -ni "s|DOCKER_REDMINE_PORT|${REDMINE_PORT}|g" "$REDMINE_UPSTREAM_CONF_FILE"
+fi
+
+GHOST_BLOG_UPSTREAM_CONF_FILE="/etc/nginx/global/ghost-blog-upstream.conf"
+if [ -f "$GHOST_BLOG_UPSTREAM_CONF_FILE" ]; then
+  sed -ni "s|DOCKER_GHOST_PORT|${GHOST_PORT}|g" "$GHOST_BLOG_UPSTREAM_CONF_FILE"
+fi
+
+#nginx -g "daemon off;"
+echo "Executing $@"
+"$@"
